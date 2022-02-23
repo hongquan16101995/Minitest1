9ef08575c231f58b3cc01ad4ddd3875ace730c1b
hongquan16101995 <66354589+hongquan16101995@users.noreply.github.com>
2022-02-23 17:37:42 +0700

    init commit

---
+++ src/main/java/cg/repository/ProductRepositoryImpl.java
@@ -0,0 +1,97 @@
+package cg.repository;
+
+import cg.model.Product;
+import org.hibernate.HibernateException;
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.hibernate.Transaction;
+import org.hibernate.cfg.Configuration;
+import org.springframework.stereotype.Repository;
+
+import javax.persistence.EntityManager;
+import javax.persistence.TypedQuery;
+import java.util.ArrayList;
+
+@Repository
+public class ProductRepositoryImpl implements IProductRepository{
+
+    private static SessionFactory sessionFactory;
+    private static EntityManager entityManager;
+
+    static {
+        try {
+            sessionFactory = new Configuration().configure("hibernate.conf.xml").buildSessionFactory();
+            entityManager = sessionFactory.createEntityManager();
+        } catch (HibernateException e) {
+            e.printStackTrace();
+        }
+    }
+
+    @Override
+    public ArrayList<Product> findAll() {
+        String queryStr = "SELECT p FROM Product AS p";
+        TypedQuery<Product> query = entityManager.createQuery(queryStr, Product.class);
+        return (ArrayList<Product>) query.getResultList();
+    }
+
+    @Override
+    public Product save(Product product) {
+        Transaction transaction = null;
+        Product origin;
+        try (Session session = sessionFactory.openSession()) {
+            transaction = session.beginTransaction();
+            if (product.getId() != 0) {
+                origin = findById(product.getId());
+                origin.setName(product.getName());
+                origin.setPrice(product.getPrice());
+                origin.setDescription(product.getDescription());
+                origin.setImage(product.getImage());
+            } else {
+                origin = product;
+            }
+            session.saveOrUpdate(origin);
+            transaction.commit();
+            return origin;
+        } catch (Exception e) {
+            e.printStackTrace();
+            if (transaction != null) {
+                transaction.rollback();
+            }
+        }
+        return null;
+    }
+
+    @Override
+    public void delete(int id) {
+        Transaction transaction = null;
+        Product origin = findById(id);
+        try (Session session = sessionFactory.openSession()) {
+            transaction = session.beginTransaction();
+            if (origin != null) {
+                session.delete(origin);
+            }
+            transaction.commit();
+        } catch (Exception e) {
+            e.printStackTrace();
+            if (transaction != null) {
+                transaction.rollback();
+            }
+        }
+    }
+
+    @Override
+    public Product findById(int id) {
+        String queryStr = "SELECT p FROM Product AS p WHERE p.id = :id";
+        TypedQuery<Product> query = entityManager.createQuery(queryStr, Product.class);
+        query.setParameter("id", id);
+        return query.getSingleResult();
+    }
+
+    @Override
+    public ArrayList<Product> findAllByName(String name) {
+        String queryStr = "SELECT p FROM Product AS p WHERE p.name LIKE :name";
+        TypedQuery<Product> query = entityManager.createQuery(queryStr, Product.class);
+        query.setParameter("name", "%" + name + "%");
+        return (ArrayList<Product>) query.getResultList();
+    }
+}
